d0496906699230c600f356509373cea0a356a2bb
Nam G. VU <namgivu@gmail.com>
2025-09-29 20:54:25 +0700

    markdownrendered_s table

--- webapp/app.py
+++ webapp/app.py
@@ -19,11 +19,43 @@ def index():
   if not NAME: NAME='todb'  # set here as notpassed NAME envvar will be passed w/ blank string
   print(f'{NAME=}', flush=True)
 
+  #region cook markdownrendered_s
+  markdownrendered_s = ''
+
+  import json
+  data = j
+  headers = list(data[0].keys())
+
+  # find max width for each column
+  col_widths = {
+    h: max(len(h), max(len(str(row[h])) for row in data))
+    for h in headers
+  }
+
+  # print header row
+  header_row = "| " + " | ".join(f"{h:<{col_widths[h]}}" for h in headers) + " |"
+  markdownrendered_s += header_row
+
+  # print separator row
+  separator_row = "| " + " | ".join("-" * col_widths[h] for h in headers) + " |"
+  markdownrendered_s += separator_row
+
+  # print data rows
+  for row in data:
+    markdownrendered_s += "| " + " | ".join(f"{str(row[h]):<{col_widths[h]}}" for h in headers) + " |"
+
+
+  #endregion cook markdownrendered_s
+
   return f'''
   <!DOCTYPE html>
   <body>
     <h1>Welcome to Flask App {NAME}!</h1>
-    <pre>{json.dumps(j, indent=2)}</pre>
+    <pre>{json.dumps(j, indent=2)}</pre>  #TODO render list of members in list j as html table
+    
+    <br>
+    <hr>
+    <pre>{markdownrendered_s}</pre>  #TODO render list of members in list j as html table
   </body>
   </html>
   '''
