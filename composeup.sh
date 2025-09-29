00d1c6e69b2ee84e7b3f1b5e19d3767d4c8754c0
Nam G. VU <namgivu@gmail.com>
2025-09-29 20:45:50 +0700

    up w/ webapp_port param to run at 8022 port

--- composeup.sh
+++ composeup.sh
@@ -3,6 +3,8 @@ cd $SH
 
 docker-compose build --no-cache
 
+export webapp_port=${webapp_port:-8000}
+
 #NAME='Nam'  docker-compose up --force-recreate -d
 #            docker-compose up --force-recreate -d
 NAME=${NAME} docker-compose up --force-recreate -d
