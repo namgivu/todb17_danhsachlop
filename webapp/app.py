cd7acec8df13173396217d59df850189a725aed8
Nam G. VU <namgivu@gmail.com>
2025-09-29 21:00:31 +0700

    markdownrendered_s table

--- webapp/app.py
+++ webapp/app.py
@@ -20,42 +20,33 @@ def index():
   print(f'{NAME=}', flush=True)
 
   #region cook markdownrendered_s
-  markdownrendered_s = ''
-
   import json
   data = j
-  headers = list(data[0].keys())
 
-  # find max width for each column
-  col_widths = {
-    h: max(len(h), max(len(str(row[h])) for row in data))
-    for h in headers
-  }
+  # Extract headers from keys
+  headers = data[0].keys()
 
-  # print header row
-  header_row = "| " + " | ".join(f"{h:<{col_widths[h]}}" for h in headers) + " |"
-  markdownrendered_s += header_row
+  # Build HTML table
+  html = "<table border='1' style='border-collapse:collapse;'>\n"
+  html += "  <tr>" + "".join(f"<th>{h}</th>" for h in headers) + "</tr>\n"
 
-  # print separator row
-  separator_row = "| " + " | ".join("-" * col_widths[h] for h in headers) + " |"
-  markdownrendered_s += separator_row
-
-  # print data rows
   for row in data:
-    markdownrendered_s += "| " + " | ".join(f"{str(row[h]):<{col_widths[h]}}" for h in headers) + " |"
+    html += "  <tr>" + "".join(f"<td>{row[h]}</td>" for h in headers) + "</tr>\n"
 
+  html += "</table>"
 
+  markdownrendered_s = html
   #endregion cook markdownrendered_s
 
   return f'''
   <!DOCTYPE html>
   <body>
     <h1>Welcome to Flask App {NAME}!</h1>
-    <pre>{json.dumps(j, indent=2)}</pre>  #TODO render list of members in list j as html table
+    <pre>{json.dumps(j, indent=2)}</pre>  
     
     <br>
     <hr>
-    <pre>{markdownrendered_s}</pre>  #TODO render list of members in list j as html table
+    <pre>{markdownrendered_s}</pre> 
   </body>
   </html>
   '''
